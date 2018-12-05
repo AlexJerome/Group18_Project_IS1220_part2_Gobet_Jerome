fde8b544444696351e3ab23f88ef91dbe0b845f5
mariongb <marion.gobet@student.ecp.fr>
2018-12-06 00:41:45 +0100

    Add Stats

--- src/Project/Stats.java
+++ src/Project/Stats.java
@@ -15,7 +15,7 @@ public class Stats {
 	public static void mostFrequentCustomer () {
 		Collections.sort(Customer.customerList, new Comparator<Customer>() {
 		    public int compare(Customer cust1, Customer cust2) {
-		        return cust1.getCountRides().compareTo(cust2.getCountRides());
+		        // return cust1.getCountRides().compareTo(cust2.getCountRides());
 		    }
 		});
 		
