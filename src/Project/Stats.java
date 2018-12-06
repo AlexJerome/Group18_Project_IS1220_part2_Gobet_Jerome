06849c0bc490a7204da55164cb88387747b449d9
mariongb <marion.gobet@student.ecp.fr>
2018-12-06 10:24:34 +0100

    Add customer's stats

--- src/Project/Stats.java
+++ src/Project/Stats.java
@@ -6,18 +6,42 @@ package Project;
 import java.util.Collections;
 import java.util.Comparator;
 
+import RidesPackage.Rides;
+
 /**
  * @author mariongobet
  *
  */
 public class Stats {
 	
-	public static void mostFrequentCustomer () {
-		Collections.sort(Customer.customerList, new Comparator<Customer>() {
-		    public int compare(Customer cust1, Customer cust2) {
-		        // return cust1.getCountRides().compareTo(cust2.getCountRides());
-		    }
-		});
-		
+	class CustomerRideComparator implements Comparator<Customer> {
+		@Override
+		public int compare(Customer c1, Customer c2) {
+			return c1.getCountRides()-c2.getCountRides();
+		}
 	}
+	
+	public void mostFrequentCustomer() {
+		Collections.sort(Customer.customerList, new CustomerRideComparator());
+		for (Customer cust : Customer.customerList) {
+			System.out.println("ID = "+cust.getCustID()+", name = "+cust.getName()+", surname = "+cust.getSurname());
+		}
+	}
+	
+	class CustomerPriceComparator implements Comparator<Customer> {
+		@Override
+		public double compare(Customer c1, Customer c2) {
+			return c1.getCountPrice()-c2.getCountPrice();
+		}
+	}
+	
+	public void mostChargedCustomer() {
+		Collections.sort(Customer.customerList, new CustomerPriceComparator());
+		for (Customer cust : Customer.customerList) {
+			System.out.println("ID = "+cust.getCustID()+", name = "+cust.getName()+", surname = "+cust.getSurname());
+		}
+	}
+	
+	
+	
 }
